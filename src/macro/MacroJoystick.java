6a51ff58bd0bb6840025cca179c2c7316f6cf68c
ThinkRedstone <ittai11@gmail.com>
2015-03-01 18:46:18 +0200

    Moved MacroJoystick to macro

--- src/macro/MacroJoystick.java
+++ src/macro/MacroJoystick.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.usfirst.frc.team2212.robot.triggers;
+package macro;
 
 import edu.wpi.first.wpilibj.Joystick;
 
