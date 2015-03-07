443e88e5430768d95fdf96822fa30df8ae26f266
Spikes-2212 <spikes.no2212@gmail.com>
2015-03-07 12:37:42 +0200

    Made iterator in Play a specific type

--- src/org/usfirst/frc/team2212/robot/commands/macro/Play.java
+++ src/org/usfirst/frc/team2212/robot/commands/macro/Play.java
@@ -5,14 +5,16 @@
  */
 package org.usfirst.frc.team2212.robot.commands.macro;
 
-import edu.wpi.first.wpilibj.command.Command;
+import static org.usfirst.frc.team2212.robot.Robot.oi;
+
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import macro.Macro;
-import static org.usfirst.frc.team2212.robot.Robot.oi;
+import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
@@ -20,52 +22,56 @@ import static org.usfirst.frc.team2212.robot.Robot.oi;
  */
 public class Play extends Command {
 
-    private Macro macro;
-    Iterator it;
+	private Macro macro;
+	Iterator<Map.Entry<Long, List[]>> it;
 
-    public Play(Macro macro) {
-        this.macro = macro;
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    }
+	public Play(Macro macro) {
+		this.macro = macro;
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-        it = macro.getData().entrySet().iterator();
-        oi.setOverride(true);
-    }
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+		it = macro.getData().entrySet().iterator();
+		oi.setOverride(true);
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-        Map.Entry<Long, List[]> pair = (Map.Entry<Long, List[]>) it.next();
-        oi.setDriverX((double) pair.getValue()[1].get(0));
-        oi.setDriverY((double) pair.getValue()[1].get(1));
-        oi.setDriverTwist((double) pair.getValue()[1].get(2));
-        for (int i = 1; i <= 12; i++) {
-            oi.setDriverButton(i, (boolean) pair.getValue()[0].get(i));
-        }
-        try {
-            Thread.sleep(pair.getKey());
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Play.class
-                    .getName()).log(Level.SEVERE, null, ex);
-        }
-        it.remove(); // avoids a ConcurrentModificationException
-    }
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		Map.Entry<Long, List[]> pair = it.next();
+		oi.setDriverX((double) pair.getValue()[1].get(0));
+		oi.setDriverY((double) pair.getValue()[1].get(1));
+		oi.setDriverTwist((double) pair.getValue()[1].get(2));
+		for (int i = 1; i <= 12; i++) {
+			oi.setDriverButton(i, (boolean) pair.getValue()[0].get(i));
+		}
+		try {
+			Thread.sleep(pair.getKey());
+		} catch (InterruptedException ex) {
+			Logger.getLogger(Play.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		it.remove(); // avoids a ConcurrentModificationException
+	}
 
-// Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return !it.hasNext();
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return !it.hasNext();
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-        oi.setOverride(false);
-    }
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+		oi.setOverride(false);
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-        end();
-    }
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+		end();
+	}
 }
